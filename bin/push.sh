99d4b4eec76d34aa9ccca523f21e7e23c834126c
Xavi <>
2022-07-26 14:51:00 +0200

    Added multi-arch

--- bin/push.sh
+++ bin/push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-docker push xjrcode/bull-board:$1
+export DOCKER_BUILDKIT=1
 
-# create latest tag
-docker image tag xjrcode/bull-board:$1 xjrcode/bull-board:latest
-docker push xjrcode/bull-board:latest
+docker buildx create --name bull-board-builder
+docker buildx use bull-board-builder
+docker buildx build --no-cache --pull --platform linux/amd64,linux/arm64 -t xjrcode/bull-board:$1 -t xjrcode/bull-board:latest --push .
+docker buildx rm bull-board-builder
